85ddc5542dfbc3b70953eede2cb93ffbe401cfba
Ruman-30 <155728528+Ruman-30@users.noreply.github.com>
2024-02-17 12:16:17 +0000

    nums and maths

---
+++ 01_basics/04_nums_and_maths.js
@@ -0,0 +1,19 @@
+const num = new Number(64.434)
+//console.log(num.toFixed(2))
+const num2  = new Boolean (-700)
+//console.log(num2)
+
+const balance = 123.8992
+//console.log(balance.toPrecision(4))
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString("en-IN"))
+
+//++++++++++++++++++++++++++ maths ++++++++++++++++++++++++++++++++++=
+
+console.log(Math.floor(Math.random()*10) + 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()* (max - min + 1)) + min)
